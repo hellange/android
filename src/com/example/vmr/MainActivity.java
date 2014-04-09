5c220bed2caa57536e59222b401bf0adcf32c796
helge langehaug <hellange@HELLANGE-M-C00B.CISCO.COM>
2014-04-09 13:12:12 +0200

    Removed irrelevant files.

--- src/com/example/vmr/MainActivity.java
+++ src/com/example/vmr/MainActivity.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.TimeZone;
 
+import android.R;
 import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Intent;
@@ -49,7 +50,7 @@ public class MainActivity extends Activity implements View.OnClickListener { // 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
 
         
         Log.d("com.example.vmr", "fisk");
@@ -190,43 +191,12 @@ count++;
         touchCount++;
         Log.d("com.example.vmr", "fisk"+touchCount+ "'"+cache);
 
-        button.setText("Touched me " + touchCount + " times   !!!!!!!!!    !"+ cache);
+       // button.setText("Touched me " + touchCount + " times   !"+ cache);
     }
     
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+    
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            return rootView;
-        }
-    }
+    
+ 
 
 }
